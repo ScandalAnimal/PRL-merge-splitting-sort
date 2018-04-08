6499e4675b2fc612573f6d2fd6b5aa38e4940780
Maros Vasilisin <xvasil02@vutbr.cz>
2018-04-08 22:58:14 +0200

    fixed output

--- test.sh
+++ test.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-    echo "Illegal number of arguments!"
+    echo "Nespravny pocet argumentov, spravny je 2"
     exit 1
 fi
 
-#vytvor soubor s cisly
-dd if=/dev/urandom bs=1 count=$1 of=numbers
+dd if=/dev/urandom bs=1 count=$1 of=numbers > /dev/null 2>&1
 
-#preklad
 mpic++ --prefix /usr/local/share/OpenMPI -o mss mss.cpp
 
-#spusteni
 mpirun --prefix /usr/local/share/OpenMPI -np $2 mss $1
 
-#uklid
 rm -f mss numbers
